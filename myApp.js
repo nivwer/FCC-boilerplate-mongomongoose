d78bbc193767556a50cd337034e052647c9e8a8e
nivwer <nivwerr@gmail.com>
2023-07-11 09:31:55 -0300

    Solution 1: Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is connected to", mongoURI);
+  })
+  .catch((error) => {
+    console.error("Error", error);
+  });
 
 let Person;
 
